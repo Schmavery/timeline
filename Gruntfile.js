a95bc9bbe4cb71289fbb31c7c8d0195312b9adf8
Benjamin San Souci <benjamin.sansouci@gmail.com>
2014-11-13 13:08:17 -0500

    Added grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+var LIVERELOAD_PORT = 35729,
+    lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT }),
+    mountFolder = function( connect, dir ) {
+      return connect.static(require('path').resolve(dir));
+    };
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+      files: ["src/**/*"],
+      tasks: ['typescript'],
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: ["src/**/*"]
+      }
+    },
+    connect: {
+      options: {
+        port: 8000,
+        hostname: '0.0.0.0'
+      },
+      livereload: {
+        options: {
+          middleware: function( connect ) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.')
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        url: 'http://localhost:<%= connect.options.port %>'
+      }
+    },
+    typescript: {
+      base: {
+        src: ['src/**/*.ts'],
+        dest: 'build/game.js',
+        options: {
+          module: 'amd', //or commonjs
+          target: 'es5', //or es3
+          basePath: 'src/',
+          sourceMap: true,
+          declaration: true
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-open');
+  grunt.loadNpmTasks('grunt-typescript');
+
+  grunt.registerTask('server', ['typescript', 'connect:livereload', 'open', 'watch']);
+
+  grunt.registerTask('default', ['server']);
+};
